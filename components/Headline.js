4a605cf23227bbf183c3bb58048582d96ef3f894
Soya Hironaka <soya.hironaka@gmail.com>
2024-11-24 18:06:25 +0900

    てすと

--- components/Headline.js
+++ components/Headline.js
@@ -2,16 +2,7 @@ import Head from "next/head";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
 
-// const geistSans = localFont({
-//   src: "../pages/fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "../pages/fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+
 
 export function Headline(props) {
   return (
