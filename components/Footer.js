15d79eb778398099852736924075821906732bc4
so-ya <sotyannpc21@gmail.com>
2024-11-25 11:28:46 +0900

    ♻️ リンクスのリファクタ

--- components/Footer.js
+++ components/Footer.js
@@ -13,51 +13,49 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+const ITEMS = [
+  { href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: '勉強',
+    image: 'File icon',
+    botton: 'Learn'
+  },
+  { href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: '例',
+    image: 'Window icon',
+    botton: 'Examples'
+  },
+  { href: "https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: '進',
+    image: 'Globe icon',
+    botton: 'Go to nextjs.org →'
+  },
+]
+
 export function Footer() {
   return (
         <footer className={classes.footer}>
+          {ITEMS.map(item => {
+            return(
+              <div key={item.href}>
+          <h3>{item.title}</h3>
           <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href={item.href}
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
               aria-hidden
               src="/file.svg"
-              alt="File icon"
+              alt={item.image}
               width={16}
               height={16}
             />
-            Learn
+            {item.botton}
           </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
+          </div>
+            )
+          })}
+          
         </footer>
   );
 }
